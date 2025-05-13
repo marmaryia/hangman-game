d7925740304aaba0bf11d4140f2d37a89a0e6be3
marmaryia <marozavamaryia@gmail.com>
2025-05-13 15:49:08 +0100

    implement lives counter

--- src/components/GamePage.tsx
+++ src/components/GamePage.tsx
@@ -3,28 +3,30 @@ import { useState, useEffect } from "react";
 
 import WordRepresentation from "./WordRepresentation";
 import Keyboard from "./Keyboard";
+import LivesCounter from "./LivesCounter";
 
 type WordRep = [] | boolean[];
 
 function GamePage() {
   const wordToGuess = useLocation().state;
   const [wordRep, setWordRep] = useState<WordRep>([]);
+  const [lives, setLives] = useState<number>(7);
 
   function handleKeyClick(letter: string) {
     let isCorrectLetter = false;
-
     wordToGuess.word.split("").forEach((wordLetter: string, i: number) => {
       if (wordLetter === letter) {
         setWordRep((current) => {
           const newRep = [...current];
           newRep[i] = true;
-
           return newRep;
         });
         isCorrectLetter = true;
       }
     });
-
+    if (!isCorrectLetter) {
+      setLives(lives > 0 ? lives - 1 : 0);
+    }
     return isCorrectLetter;
   }
 
@@ -37,6 +39,7 @@ function GamePage() {
   }
   return (
     <section>
+      <LivesCounter lives={lives} />
       <WordRepresentation word={wordToGuess.word} wordRep={wordRep} />
       <Keyboard handleKeyClick={handleKeyClick} />
     </section>
