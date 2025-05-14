528b761501ae5ee8ed2a2c2534efd9eab7d5c6d9
marmaryia <marozavamaryia@gmail.com>
2025-05-14 11:31:56 +0100

    extract popup for choosing a word into a component

---
+++ src/components/ChooseWordPopup.tsx
@@ -0,0 +1,54 @@
+import { useContext, useState } from "react";
+import Popup from "reactjs-popup";
+import { WordContext } from "../contexts/WordContext";
+import { data } from "../assets/dictionary.ts";
+
+function ChooseWordPopup({
+  popupOpen,
+  setPopupOpen,
+}: {
+  popupOpen: boolean;
+  setPopupOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  const { setWordToGuess } = useContext(WordContext);
+  const [userChosenWord, setUserChosenWord] = useState("");
+
+  function handleWordSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setWordToGuess({
+      word: userChosenWord,
+      definition: data[userChosenWord] ? data[userChosenWord] : null,
+    });
+    setUserChosenWord("");
+    setPopupOpen(false);
+  }
+
+  return (
+    <Popup
+      modal
+      position={"center center"}
+      closeOnDocumentClick={true}
+      open={popupOpen}
+    >
+      <form action="submit" onSubmit={handleWordSubmit}>
+        <label htmlFor="wordToGuess">
+          One of the players chooses a word (3 to 13 characters):
+        </label>
+        <br />
+        <input
+          type="password"
+          name="wordToGuess"
+          id="wordToGuess"
+          minLength={3}
+          maxLength={13}
+          onChange={(e) => {
+            setUserChosenWord(e.target.value.toUpperCase());
+          }}
+        />
+        <button type="submit">Confirm</button>
+      </form>
+    </Popup>
+  );
+}
+
+export default ChooseWordPopup;
